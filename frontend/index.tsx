0f324a0d6b0840e405d4eafa6180835f5c8edfae
dadncingmonkey <zhongjohnny123@gmail.com>
2025-08-02 10:03:26 -0400

    qfkhckqe

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -19,7 +19,7 @@ const ChatBot: React.FC = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/chat', {
+      const response = await fetch('http://localhost:3000/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: userInput }),
